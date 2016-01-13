56386736c03be30b192cfba12c99c76b35e27bbc
Kévin Casagrande <kcasagrande@norsys.fr>
2016-01-13 13:47:30 +0100

    Unit test for FileCorpus.

--- test/FileCorpusTest.scala
+++ test/FileCorpusTest.scala
@@ -1,5 +1,12 @@
 package wordgen
 
-class FileCorpusTest {
-
+class FileCorpusTest extends TestCase {
+  "A FileCorpus" must "give the words in the file, in the same order" in {
+    val corpusIterator = new FileCorpus( "test-assets/simple.txt" ).iterator
+    assert( corpusIterator.next() == "abri", "The first word should be \"abri\"." )
+    assert( corpusIterator.next() == "loup", "The second word should be \"loup\"." )
+    assert( corpusIterator.next() == "nem", "The third word should be \"nem\"." )
+    assert( corpusIterator.next() == "ys", "The fourth word should be \"ys\"." )
+  }
 }
+
---
+++ test/TestCase.scala
@@ -0,0 +1,5 @@
+package wordgen
+
+import org.scalatest.FlatSpec
+
+abstract class TestCase extends FlatSpec
